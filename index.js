27b585d607701c642491a4af29d00ed5cab2fc04
kost90 <kostdonc90@gmail.com>
2023-05-12 16:16:05 +0300

    created localstorage functional, edit addtask function & donetaskfunction

--- index.js
+++ index.js
@@ -1,48 +1,88 @@
 // Create variables with html tags
-// const listContainer = document.createElement('div');
-// listContainer.classList.add('listcontainer');
 let btnNewTask = document.getElementById('btnNewTask');
 let inputTask = document.getElementById('inputTask');
 let listcontainer = document.querySelector('.listcontainer');
 
 let tasks = [];
 
+//Create function to render saved data from localstorage
+renderPage();
+
+function renderPage() {
+    let tasksData = localStorage.getItem('tasks');
+    tasks = JSON.parse(tasksData);
+    tasks.forEach((task) => {
+        const div = document.createElement('div');
+        div.classList.add('div-container');
+        const checkbox = document.createElement('input');
+        checkbox.classList.add('checked');
+        //how to take property of checkbox?
+        checkbox.setAttribute('type', 'checkbox');
+        const removbtn = document.createElement('button');
+        removbtn.classList.add('removbtn');
+        removbtn.setAttribute('data-action', 'delete')
+        removbtn.textContent = 'delete';
+        const span = document.createElement('span');
+        span.textContent = task.text;
+        div.append(span, checkbox, removbtn);
+        const li = document.createElement('li');
+        li.classList.add('notdone');
+        li.setAttribute('id', `${task.id}`);
+        li.append(div);
+        listcontainer.append(li);
+    });
+}
+
+
 //Create function addtask to listcontainer
 
 btnNewTask.addEventListener('click', addTask);
 
-function addTask(e){
+function addTask(e) {
     e.preventDefault();
     let newTask = {
         text: inputTask.value,
         checked: false,
+        id: Date.now(),
     }
-    const div = document.createElement('div');
-    div.classList.add('div-container');
-    const checkbox = document.createElement('input');
-    checkbox.classList.add('checked');
-    checkbox.setAttribute('type', 'checkbox');
-    const removbtn = document.createElement('button');
-    removbtn.classList.add('removbtn');
-    removbtn.setAttribute('data-action', 'delete')
-    removbtn.textContent = 'delete';
-    const span = document.createElement('span');
-    span.textContent = newTask.text;
-    div.append(span,checkbox,removbtn);
-    const li = document.createElement('li');
-    li.append(div);
-    listcontainer.append(li);
-    inputTask.value = '';
-    return  tasks.push(newTask);
+    if (newTask.text === '') {
+        return
+    } else {
+        const div = document.createElement('div');
+        div.classList.add('div-container');
+        const checkbox = document.createElement('input');
+        checkbox.classList.add('checked');
+        //how to take property of checkbox?
+        checkbox.setAttribute('type', 'checkbox');
+        const removbtn = document.createElement('button');
+        removbtn.classList.add('removbtn');
+        removbtn.setAttribute('data-action', 'delete')
+        removbtn.textContent = 'delete';
+        const span = document.createElement('span');
+        span.textContent = newTask.text;
+        span.setAttribute('id', `${newTask.id}`);
+        div.append(span, checkbox, removbtn);
+        const li = document.createElement('li');
+        li.classList.add('notdone');
+        li.setAttribute('id', `${newTask.id}`);
+        li.append(div);
+        listcontainer.append(li);
+        inputTask.value = '';
+        tasks.push(newTask);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 }
 
 //create function delete task
 
 listcontainer.addEventListener('click', removeTask);
 
-function removeTask(e){
-    if (e.target.dataset.action === 'delete'){
+function removeTask(e) {
+    if (e.target.dataset.action === 'delete') {
         const parenttag = e.target.closest('li');
+        const id = Number(parenttag.id);
+        tasks = tasks.filter((task) => task.id !== id);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
         parenttag.remove()
     }
 }
@@ -50,11 +90,34 @@ function removeTask(e){
 //create function done task
 listcontainer.addEventListener('click', doneTask);
 
-function doneTask(e){
-    if (e.target.checked === true){
+function doneTask(e) {
+    if (e.target.checked === true) {
+        //how to check property of checkbox?
         const parenttag = e.target.closest('li');
-        parenttag.classList.add('done')
+        parenttag.classList.toggle('done')
     }
 }
+
+//crete function edit task
+
+// listcontainer.addEventListener('click', editTask);
+//
+// function editTask(e) {
+//     if (e.target.classList.contains('notdone')){
+//         const newInput = document.createElement('input')
+//         newInput.setAttribute('type', 'text');
+//         const parenttag = e.target.closest('span');
+//         const id = Number(parenttag.id);
+//         tasks.forEach((task) => {
+//             if (task.id === id){
+//                 parenttag.append(newInput);
+//                 task.text = newInput.value;
+//                 localStorage.setItem('tasks', JSON.stringify(tasks));
+//             }
+//         });
+//     }
+// }
+
+
 
 
